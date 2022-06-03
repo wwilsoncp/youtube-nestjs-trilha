cc7e382e8868dc78cf84fa97f5682b92af986b02
Wilson Paiva <paiva.wilson@dedalus.com>
2022-06-03 19:09:41 -0300

    Adding swagger configuration and users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirstModule } from './first/first.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [FirstModule],
+  imports: [FirstModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Trilha NestJS')
+    .setDescription('Descrição da API de trilha com nestjs')
+    .setVersion('1.0')
+    .addTag('trilha')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // http://localhost:8000/api vai exibir a documentação, porém precisa criar os DTOs e as anotações necessárias nos métodos
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
